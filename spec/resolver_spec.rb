acf1876a3d95cce7bb92c6cd7431a72df2ccdb97
Rafał Rzepecki <divided.mind@gmail.com>
2017-07-02 19:00:05 +0200

    Refactor specs a bit
    For better error messages, clearer code and easier extensibility.

--- spec/resolver_spec.rb
+++ spec/resolver_spec.rb
@@ -3,26 +3,8 @@ require 'spec_helper'
 include Conjur::Policy
 
 describe Resolver do
-  let(:fixture) { YAML.load(File.read(filename), filename) }
-  let(:account) { fixture['account'] || "the-account" }
-  let(:ownerid) { fixture['ownerid'] || "rspec:user:default-owner" }
-  let(:namespace) { fixture['namespace'] }
-  let(:policy) { Conjur::Policy::YAML::Loader.load(fixture['policy']) }
-  let(:resolve) {
-    Resolver.resolve(policy, account, ownerid, namespace)
-  }
-  before {
-    allow(Conjur).to receive(:configuration).and_return double(:configuration, account: account)
-  }
-  subject { resolve.to_yaml }
-  
-  shared_examples_for "verify resolver" do
-    it "matches expected YAML" do
-      expect(subject).to eq(fixture['expectation'])
-    end
-  end
-    
   fixtures_dir = File.expand_path("resolver-fixtures", File.dirname(__FILE__))
+
   Dir.chdir(fixtures_dir) do
     files = if env = ENV['POLICY_FIXTURES']
       env.split(',')
@@ -31,9 +13,18 @@ describe Resolver do
     end
 
     files.each do |file_example_name|
-      describe file_example_name do
-        let(:filename) { File.expand_path(file_example_name, fixtures_dir) }
-        it_should_behave_like "verify resolver"
+      example file_example_name do
+        filename = File.expand_path file_example_name, fixtures_dir
+        fixture = YAML.load File.read(filename), filename
+
+        account = fixture['account'] || "the-account"
+        ownerid = fixture['ownerid'] || "rspec:user:default-owner"
+        namespace = fixture['namespace']
+
+        allow(Conjur).to receive(:configuration).and_return double(:configuration, account: account)
+
+        policy = Conjur::Policy::YAML::Loader.load fixture['policy']
+        expect(Resolver.resolve(policy, account, ownerid, namespace).to_yaml).to eq(fixture['expectation'])
       end
     end
   end
